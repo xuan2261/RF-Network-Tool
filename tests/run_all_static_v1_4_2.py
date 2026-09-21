41915ff4622291d3e3a1c5e17b473effbc4c1999
Bui Thanh Xuan <buithanhxuan2261@gmail.com>
2026-09-21 10:12:46 +0700

    test: activate IPv6 NDP contract

--- tests/run_all_static_v1_4_2.py
+++ tests/run_all_static_v1_4_2.py
@@ -6,7 +6,7 @@ files=[
  'core_contract_v1_3_1.py','evidence_contract_v1_3_1.py','history_contract_v1_3_1.py','profile_policy_contract_v1_3_1.py',
  'runid_contract_v1_3_1.py','scan_state_contract_v1_3_1.py','target_persistence_contract_v1_3_1.py',
  'async_ping_contract_v1_4_0.py','oui_task_contract_v1_4_0.py','responsiveness_contract_v1_4_0.py','monitoring_contract_v1_4_0.py',
- 'runtime_integrity_contract_v1_4_2.py','ci_workflow_contract_v1_4_2.py','real_machine_harness_contract_v1_4_2.py','security_audit_v1_4_2.py','static_audit_v1_4_2.py'
+ 'runtime_integrity_contract_v1_4_2.py','ipv6_ndp_contract_v1_4_2.py','ci_workflow_contract_v1_4_2.py','real_machine_harness_contract_v1_4_2.py','security_audit_v1_4_2.py','static_audit_v1_4_2.py'
 ]
 failed=[]
 for f in files:
